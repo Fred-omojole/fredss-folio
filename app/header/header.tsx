02bb137792b3d4adbd0972fcb06db4a793949cb9
freddy.dev <omoyeleomojole2004@gmail.com>
2024-08-10 20:32:39 +0100

    fix

--- app/header/header.tsx
+++ app/header/header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 import Link from "next/link";
 import base from "@/public/base.png";
-import NavMobile from "../navMobile/NavMobile";
+import NavMobile from "@/app/navMobile/NavMobile";
 
 const Header = () => {
   return (
